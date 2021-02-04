ae77e592d4ad4cd1753ed6dd013b7a2dd10d90c8
helman101 <helmanandresortegon@gmail.com>
2021-02-04 11:55:31 +0100

    create through table, create associations, edit User and Event views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_04_091917) do
+ActiveRecord::Schema.define(version: 2021_02_04_102422) do
+
+  create_table "attendances", force: :cascade do |t|
+    t.integer "attendee_id"
+    t.integer "attended_event_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "events", force: :cascade do |t|
     t.string "name"
