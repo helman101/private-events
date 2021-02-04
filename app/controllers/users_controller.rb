d314df5b6f3bfeaa9b2a741ec35383a6ce45e94e
helman101 <helmanandresortegon@gmail.com>
2021-02-04 09:39:18 +0100

    generate Users controller and views

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:current_user_id] = @user.id
+      redirect_to @user, notice: 'You are successfully Sing up'
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :users, except:[:destroy, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'users#new'
 end
