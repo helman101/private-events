6489d7fb51af172ac860e16ea3b7cb820c7c81c3
helman101 <helmanandresortegon@gmail.com>
2021-02-04 10:00:19 +0100

    generate Events controller

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,13 @@
+class EventsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'events/new'
+  get 'events/create'
+  get 'events/show'
+  get 'events/index'
   resources :users, except:[:destroy, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'users#new'
