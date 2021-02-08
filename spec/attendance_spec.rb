78c21ba2e964d236cdefb62e6791b329a4afb645
helman101 <helmanandresortegon@gmail.com>
2021-02-08 16:07:17 +0100

    create attendance tests file and refactor attendance tests

---
+++ spec/attendance_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+describe Event, type: :model do
+  let(:att) { Attendance.new }
+  let(:user) { User.create(name: 'user') }
+  let(:evt) { Event.create(name: 'Test', description: 'Test description', date: Date.current) }
+
+  context 'validate models associations' do 
+    it 'returns true when do have attendee and attended_event' do
+      att.attendee = user
+      att.attended_event = evt
+      expect(att.valid?).to be true
+    end
+
+    it 'returns false when doesn\'t have attendee or attended_event' do
+      att.attended_event = evt
+      expect(att.valid?).to be false
+    end
+  end
+
+  context 'relations work propertly' do
+    it 'attendee is equal to user' do
+      att.attendee = user
+      att.attended_event = evt
+      expect(att.attendee).to eq(user)
+    end
+
+    it 'attended_event is equal to event' do
+      att.attendee = user
+      att.attended_event = evt
+      expect(att.attended_event).to eq(evt)
+    end
+  end
+end
